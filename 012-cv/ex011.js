926180abe4c25dec2494b6378a2b4ee1dc7838c6
Alan Colli <150622900+alan-colli@users.noreply.github.com>
2024-03-03 17:53:57 -0300

    att

---
+++ 012-cv/ex011.js
@@ -0,0 +1,8 @@
+let idade = 64;
+if (idade < 16) {
+  console.log("Nao vota");
+} else if (idade < 18 || idade > 65) {
+  console.log("Voto opcional");
+} else {
+  console.log("Voto obrigatorio");
+}
