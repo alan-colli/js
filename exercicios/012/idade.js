7e4737575f60d7f22097994b4807935f6377e8aa
Alan Colli <150622900+alan-colli@users.noreply.github.com>
2024-03-13 20:32:12 -0300

    Update idade.js

--- exercicios/012/idade.js
+++ exercicios/012/idade.js
@@ -3,13 +3,18 @@ function verificar() {
   let ano = Number(ttn.value);
   let data = new Date();
   let atual = data.getFullYear();
-  let idade = atual - ano;
+
   let res = window.document.querySelector("div#saidas");
 
-  let checkboxes = window.document.querySelector(
-    'input[name="genero"]:checked'
-  );
-  let sexo = [];
-  sexo.push(checkboxes.value);
-  res.innerHTML = `<p>Sua idade é ${idade} e seu sexo é ${sexo}`;
+  if (ano.length == 0 || ano > atual) {
+    window.alert("[ERRO] Verifique os dados!");
+  } else {
+    let idade = atual - ano;
+    let checkboxes = window.document.querySelector(
+      'input[name="genero"]:checked'
+    );
+    let sexo = [];
+    sexo.push(checkboxes.value);
+    res.innerHTML = `<p>Sua idade é ${idade} e seu sexo é ${sexo}`;
+  }
 }
