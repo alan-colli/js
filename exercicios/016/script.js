a5e34b8e681cdb67fc28e0a90105f259d8c30df6
Alan Colli <150622900+alan-colli@users.noreply.github.com>
2024-03-14 19:48:55 -0300

    att

--- exercicios/016/script.js
+++ exercicios/016/script.js
@@ -9,9 +9,7 @@ let passo = Number(ttnp.value);
 let res = window.document.querySelector("div#saidas");
 let numero = inicio;
 function contar() {
-  for (inicio; inicio >= fim; passo++) {
-    numero = inicio;
-    res.innerHTML = `<p>${numero}</p>`;
-    numero = numero + passo;
+  for () {
+    
   }
 }
